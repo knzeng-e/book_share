0285cf24d624a173a253a6c24ba7d1e30c7475c8
knzeng-e <kevin.nzeng@gmail.com>
2021-01-23 20:35:40 +0100

    Adding a description blank page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='container'>
+            <h3> Partage de livres</h3>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias soluta repudiandae, fuga asperiores voluptatum sequi, 
+            sed nobis maxime, saepe accusantium animi dolorum. Delectus temporibus dicta nam officiis explicabo aliquid dolorum.
+        </div>
+    )
+}
+
+export default About
